543a8bd276f365501daef0699e9d396c3fd1a435
Noe Osorio <osorionoe@gmail.com>
2023-12-07 17:58:20 -0600

    Clean prints

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -2,10 +2,8 @@ import firebase_admin
 import os
 from firebase_admin import credentials, firestore
 
-print(os.getcwd())
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cred_path = os.path.join(dir_path, 'firebase_settings.json')
-print(cred_path)
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
